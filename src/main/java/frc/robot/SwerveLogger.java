cab7c7cd5da550786ca2079287537ca455a5024e
oliverstivers <oliverstivers@gmail.com>
2025-01-18 10:37:58 -0800

    cleaner

--- src/main/java/frc/robot/SwerveLogger.java
+++ src/main/java/frc/robot/SwerveLogger.java
@@ -1,32 +1,32 @@
 package frc.robot;
 
+import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.swerve.SwerveDrivetrain;
 
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.epilogue.NotLogged;
-import edu.wpi.first.wpilibj.motorcontrol.Talon;
 
 @Logged
 public class SwerveLogger {
     @NotLogged
-    SwerveDrivetrain dt;
+    SwerveDrivetrain<TalonFX, TalonFX, CANcoder> dt;
 
     TalonFX frontLeftDrive, frontRightDrive, backLeftDrive, backRightDrive;
     TalonFX frontLeftSteer, frontRightSteer, backLeftSteer, backRightSteer;
 
-    public SwerveLogger(SwerveDrivetrain dt) {
+    public SwerveLogger(SwerveDrivetrain<TalonFX, TalonFX, CANcoder> dt) {
         this.dt = dt;
 
-        frontLeftDrive = (TalonFX) dt.getModule(0).getDriveMotor();
-        frontRightDrive = (TalonFX) dt.getModule(1).getDriveMotor();
-        backLeftDrive = (TalonFX) dt.getModule(2).getDriveMotor();
-        backRightDrive = (TalonFX) dt.getModule(3).getDriveMotor();
+        frontLeftDrive = dt.getModule(0).getDriveMotor();
+        frontRightDrive = dt.getModule(1).getDriveMotor();
+        backLeftDrive = dt.getModule(2).getDriveMotor();
+        backRightDrive = dt.getModule(3).getDriveMotor();
 
-        frontLeftSteer = (TalonFX) dt.getModule(0).getSteerMotor();
-        frontRightSteer = (TalonFX) dt.getModule(1).getSteerMotor();
-        backLeftSteer = (TalonFX) dt.getModule(2).getSteerMotor();
-        backRightSteer = (TalonFX) dt.getModule(3).getSteerMotor();
+        frontLeftSteer = dt.getModule(0).getSteerMotor();
+        frontRightSteer = dt.getModule(1).getSteerMotor();
+        backLeftSteer = dt.getModule(2).getSteerMotor();
+        backRightSteer = dt.getModule(3).getSteerMotor();
     }
 
 }
